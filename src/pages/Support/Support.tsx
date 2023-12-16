1743f7909e356b86215d03f3b0c8881a70b457eb
vngson <Soncuhkb1111@gmail.com>
2023-12-16 08:53:26 +0700

    "update UI support"

--- src/pages/Support/Support.tsx
+++ src/pages/Support/Support.tsx
@@ -13,18 +13,11 @@ function Support() {
                 <div className="text-blue-900 text-2xl font-black font-['Roboto']">Admin</div>
             </div>
             <div className="flex w-full justify-center items-center"><div className="w-[360px] h-[0px] border-[1px] border-blue-900"></div></div>
-            <span className="px-[20px] py-[8px] bg-blue-900 rounded-[25px] justify-center items-center flex text-white text-[20px] font-normal font-['Roboto'] tracking-wide">Hello!</span>
-            <div className="flex-col justify-center items-center gap-2.5 flex">
-                <div className="w-[390px] h-10 relative">
-                    <div className="w-[390px] h-10 left-0 top-0 absolute bg-blue-900 rounded-[25px]" />
-                    <div className="left-[14px] top-[8px] absolute text-white text-[20px] font-normal font-['Roboto']">Can you help me with the contract issue?</div>
-                </div>
-            </div>
-            <div className="px-7 py-2 bg-neutral-200 rounded-[25px] justify-center items-center gap-2.5 flex">
-                <div className="text-zinc-800 text-[20px] font-normal font-['Roboto'] tracking-wide">Hey There!</div>
-            </div>
-            <div className="px-[29px] py-2 bg-neutral-200 rounded-[25px] justify-center items-center gap-2.5 flex">
-                <div className="text-zinc-800 text-[20px] font-normal font-['Roboto'] tracking-wide">How are you?</div>
+            <div className = "h-[300px] w-full overflow-y-auto flex flex-col justify-end">
+                <span className="w-fit max-w-[300px] h-fit px-[20px] py-[8px] bg-blue-900 mb-[10px] rounded-[25px] justify-center items-center flex text-white text-[20px] font-normal font-['Roboto'] tracking-wide">Hello!</span>
+                <span className="w-fit max-w-[300px] h-fit px-[20px] py-[8px] bg-blue-900 mb-[10px] rounded-[25px] justify-center items-center flex text-white text-[20px] font-normal font-['Roboto'] tracking-wide">Can you help me with the contract issue?</span>
+                <span className="w-fit max-w-[300px] h-fit px-[20px] py-[8px] text-zinc-800 justify-start items-center float-wight flex text-[20px] font-normal font-['Roboto'] tracking-wide ml-auto mr-[30px] mb-[10px]">Hey There!</span>
+                <span className="w-fit max-w-[300px] h-fit px-[20px] py-[8px] text-zinc-800 justify-start items-center float-wight flex text-[20px] font-normal font-['Roboto'] tracking-wide ml-auto mr-[30px] mb-[10px]">How are you?</span>
             </div>
             <div className="h-fit w-[360px] mx-[20px] mb-[20px] relative bg-gray-200 rounded-[15px] border-2 border-blue-900 justify-center items-center gap-[34px] flex select-none">
                 <textarea
